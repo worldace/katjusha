6f289786da65724a1a730f3ff44b29c5643859d1
worldace <tokaiknight@yahoo.co.jp>
2019-12-19 16:57:29 +0900

    no message

--- test/katjusha.js
+++ test/katjusha.js
@@ -1,17 +1,43 @@
 
 板一覧.onclick = function(event){
-    event.preventDefault();
+    event.preventDefault()
     if(event.target.tagName !== 'A'){
-        return;
+        return
     }
     
     const selected = document.querySelector("#板一覧 [data-selected]");
     if(selected){
-        delete selected.dataset.selected;
+        delete selected.dataset.selected
     }
-    event.target.dataset.selected = 'selected';
+    event.target.dataset.selected = 'selected'
 
-    const dir  = event.target.href.split('/');
-    const 板名 = dir[dir.length-2];
-    console.dir(板名);
+    const dir  = event.target.href.split('/')
+    const 板名 = dir[dir.length-2]
+}
+
+
+レス投稿ボタン.onclick = function (event){
+    if(katjusha.dataset.投稿フォーム){
+        return
+    }
+
+    投稿フォーム_タイトル欄.value = ''
+    投稿フォーム_名前欄.value     = ''
+    投稿フォーム_メール欄.value   = ''
+    投稿フォーム_本文欄.value     = ''
+
+    投稿フォーム_タイトル欄.disabled = true
+
+    katjusha.dataset.投稿フォーム = 'レス'
+    投稿フォーム_本文欄.focus()
+}
+
+
+投稿フォーム_キャンセルボタン.onclick = function (event){
+    delete katjusha.dataset.投稿フォーム
+}
+
+
+投稿フォーム_閉じるボタン.onclick = function (event){
+    delete katjusha.dataset.投稿フォーム
 }
