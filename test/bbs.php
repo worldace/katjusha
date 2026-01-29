ee6fc280c371e6312931a97ee74cf384ba521bab
tokaiknight <tokaiknight@yahoo.co.jp>
2026-01-29 12:30:55 +0900

    no message

--- test/bbs.php
+++ test/bbs.php
@@ -1,394 +1,281 @@
 <?php
-/*
-二重書き込み
-*/
+
+ini_set("error_log", "/www/phperror.txt");
+$setting = setting_load();
+$c       = (object)["isMonazilla" => str_contains($_SERVER['HTTP_USER_AGENT']??'', 'Monazilla')];
 
 mb_substitute_character('entity');
-
-if(!$_POST){
-    parse_str(file_get_contents('php://input'), $_POST);
-}
-if(post('time')){
-    mb_convert_variables('utf-8', 'sjis', $_POST);
-}
+if(!$_POST) parse_str(file_get_contents('php://input'), $_POST);
+if($c->isMonazilla) mb_convert_variables('utf-8', 'sjis', $_POST);
 
 
-define('REFERER', $_SERVER['HTTP_REFERER'] ?? '');
-define('RES', (bool)post('key'));
-define('BBS', post('bbs'));
-define('KEY', post('key') ?: $_SERVER['REQUEST_TIME']);
-define('FROM', post('FROM'));
-define('MAIL', post('mail'));
-define('SUBJECT', post('subject'));
-define('MESSAGE', post('MESSAGE'));
-define('PATH', sprintf('%s/../%s', __DIR__, BBS));
-define('SET', (function(){
-    include __DIR__.'/setting.php';
-    return get_defined_vars();
-})());
-define('ADMIN', (function (){
-    $i = array_search(MAIL, array_column(SET['admin'], 'password'));
-    return is_int($i) ? SET['admin'][$i] : [];
-})());
+$c->isRes   = preg_match("/\d/", $_POST['key'] ?? '');
+$c->bbs     = post('bbs');
+$c->key     = post('key') ?: $_SERVER['REQUEST_TIME'];
+$c->from    = post('FROM');
+$c->mail    = post('mail');
+$c->subject = post('subject');
+$c->message = post('MESSAGE');
+
+$c->bbsDir      = __DIR__."/../$c->bbs";
+$c->subjectFile = "$c->bbsDir/subject.txt";
+$c->datFile     = "$c->bbsDir/dat/$c->key.dat";
+$c->kakoFile    = "$c->bbsDir/kako/".substr($c->key,0,3)."/$c->key.dat";
+$c->admin       = setting_admin($setting, $c->mail);
 
 
-
-if(ADMIN and method_exists('Maintenance', FROM)){
-    error(Maintenance::{FROM}(PATH, MESSAGE, KEY));
+if($c->admin and is_callable("管理$c->from")){
+    error("管理$c->from"($c));
 }
 
 
-if($_SERVER['HTTP_HOST'] !== parse_url(REFERER, PHP_URL_HOST)){
+if(!$c->isMonazilla and $_SERVER['HTTP_SEC_FETCH_SITE'] !== 'same-origin'){
     error('リファラが不正です');
 }
-if(!BBS){
+if(!$c->bbs){
     error('bbsが存在しません');
 }
-if(preg_match('/[\W]/', BBS)){
+if(preg_match('/[\W]/', $c->bbs)){
     error('bbsが不正です');
 }
-if(!Subject::exists(PATH)){
+if(!file_exists($c->bbsDir)){
     error('板が存在しません');
 }
-if(RES and preg_match('/[\D]/', KEY)){
+if(preg_match('/[\D]/', $c->key)){
     error('keyが不正です');
 }
-if(RES and !Thread::exists(PATH, KEY) and Thread::is_kako(PATH, KEY)){
-    error('このスレは過去ログなので書き込めません');
+if($c->isRes and !file_exists($c->datFile)){
+    error('このスレは書き込めません');
 }
-if(RES and !Thread::exists(PATH, KEY) and !Thread::is_kako(PATH, KEY)){
-    error('このスレは存在しません');
-}
-if(!RES and !SUBJECT){
-    error('タイトルを入力してください');
-}
-if(!RES and !isUTF8(SUBJECT)){
-    error('文字コードが不正です');
-}
-if(!RES and strlen(SUBJECT) > 96){
-    error('タイトルが長すぎます');
-}
-if(strlen(FROM) > 32){
+if(strlen($c->from) > 32){
     error('名前が長すぎます');
 }
-if(!isUTF8(FROM)){
+if(!isUTF8($c->from)){
     error('文字コードが不正です');
 }
-if(strlen(MAIL) > 64){
+if(strlen($c->mail) > 64){
     error('メールが長すぎます');
 }
-if(!isUTF8(MAIL)){
+if(!isUTF8($c->mail)){
     error('文字コードが不正です');
 }
-if(!MESSAGE){
+if(!$c->message){
     error('本文を入力してください');
 }
-if(strlen(MESSAGE) > 4096){
+if(strlen($c->message) > 4096){
     error('本文が長すぎます');
 }
+if(!$c->isRes and !$c->subject){
+    error('タイトルを入力してください');
+}
+if(!$c->isRes and !isUTF8($c->subject)){
+    error('文字コードが不正です');
+}
+if(!$c->isRes and strlen($c->subject) > 96){
+    error('タイトルが長すぎます');
+}
+if(!$c->isRes and file_exists($c->datFile)){
+    error('再度スレを立ててください');
+}
 
 
+$c->from    = dat_from($c->from, $setting['nanashi'], $c->admin);
+$c->mail    = dat_mail($c->mail);
+$c->message = dat_message($c->message);
+$c->subject = dat_subject($c->subject);
 
-if(RES){
-    Res::save(PATH, KEY, Res::from(FROM), Res::mail(MAIL), Res::message(MESSAGE)) ? ok(BBS, KEY) : error('レス書き込みエラー');
+
+if($c->isRes){
+    file_edit($c->subjectFile, 'res_save') ? ok($c->bbs, $c->key) : error('レス書き込みエラー');
 }
 else{
-    Thread::save(PATH, KEY, Res::from(FROM), Res::mail(MAIL), Res::message(MESSAGE), Res::subject(SUBJECT)) ? ok(BBS, KEY) : error('スレッド書き込みエラー');
+    file_edit($c->subjectFile, 'thread_save') ? ok($c->bbs, $c->key) : error('スレッド書き込みエラー');
+}
+
+exit;
+
+
+
+function res_save($content){
+    global $c;
+
+    $line = subject_select($content, $c->key);
+    if(!$line) error('このスレッドは存在しません');
+    if($line->num >= 1000)  error('このスレッドにはこれ以上書き込めません');
+
+    $date = dat_date();
+    $dat  = mb_convert_encoding("$c->from<>$c->mail<>$date<> $c->message <>\n", 'sjis', 'utf-8');
+    file_put_contents($c->datFile, $dat, LOCK_EX|FILE_APPEND);
+
+    $line->num++;
+    array_splice($content, $line->index, 1);
+    array_unshift($content, "$c->key.dat<>$line->subject ($line->num)\n");
+
+    return $content;
+}
+
+function res_delete($dat, $kako, $num){
+    $file = file_exists($dat) ? $dat : $kako;
+
+    file_edit($file, function($content) use($num){
+        $content[$num-1] = mb_convert_encoding("あぼーん<>あぼーん<>あぼーん<>あぼーん<>\n", 'sjis', 'utf-8');
+        return $content;
+    });
 }
 
 
+function thread_save($content){
+    global $c;
 
+    $date = dat_date();
+    $dat  = mb_convert_encoding("$c->from<>$c->mail<>$date<> $c->message <>$c->subject\n", 'sjis', 'utf-8');
+    $txt  = mb_convert_encoding("$c->key.dat<>$c->subject (1)\n", 'sjis', 'utf-8');
 
-class Subject{
-    static function path($path){
-        return "$path/subject.txt";
+    file_put_contents($c->datFile, $dat, LOCK_EX);
+    array_unshift($content, $txt);
+
+    return $content;
+}
+
+function thread_delete($bbsDir, $key){
+    $datFile = "$bbsDir/dat/$key.dat";
+    if(file_exists($datFile)){
+        unlink($datFile);
+        subject_delete($bbsDir, $key);
     }
-
-
-    static function exists($path){
-        return file_exists(self::path($path));
-    }
-
-
-    static function select($file, $key){
-        $marker = "$key.";
-        foreach($file as $k => $v){
-            if(strpos($v, $marker) === 0){
-                preg_match('/<>(.+?) \((\d+)\)$/', $v, $m);
-                return (object)['index'=>$k, 'key'=>$key, 'subject'=>$m[1], 'num'=>$m[2]];
-            }
-        }
-    }
-
-
-    static function delete($path, $key){
-        $marker = "$key.";
-        file_edit(self::path($path), function($file) use($marker){
-            return array_filter($file, function($line) use($marker){ return strpos($line, $marker) !== 0; });
-        });
+    else{
+        unlink("$bbsDir/kako/".substr($key,0,3)."/$key.dat");
     }
 }
 
-
-
-class Thread{
-    static function path($path, $key){
-        return "$path/dat/$key.dat";
+function thread_move($bbsDir, $key){
+    $datFile  = "$bbsDir/dat/$key.dat";
+    $kakoFile = "$bbsDir/kako/".substr($key,0,3)."/$key.dat";
+    $kakoDir  = dirname($kakoFile);
+    if(!is_dir($kakoDir)){
+        mkdir($kakoDir, 0777, true);
     }
-
-
-    static function exists($path, $key){
-        return file_exists(self::path($path, $key));
-    }
-
-
-    static function kako_path($path, $key){
-        return sprintf('%s/kako/%s/%s.dat', $path, substr($key,0,3), $key);
-    }
-
-
-    static function is_kako($path, $key){
-        return file_exists(self::kako_path($path, $key));
-    }
-
-
-    static function save($path, $key, $from, $mail, $message, $subject){
-        if(self::exists($path, $key)){
-            error('再度スレを立ててください');
-        }
-        $date = Res::date($key);
-        $dat  = mb_convert_encoding("$from<>$mail<>$date<> $message <>$subject\n", 'sjis', 'utf-8');
-        $txt  = mb_convert_encoding("$key.dat<>$subject (1)\n", 'sjis', 'utf-8');
-
-        return file_edit(Subject::path($path), function($file) use($path, $key, $dat, $txt){
-            file_put_contents(self::path($path, $key), $dat, LOCK_EX);
-            array_unshift($file, $txt);
-            return $file;
-        });
-    }
-
-
-    static function delete($path, $key){
-        if(self::exists($path, $key)){
-            unlink(self::path($path, $key));
-            Subject::delete($path, $key);
-        }
-        else{
-            unlink(self::kako_path($path, $key));
-        }
-    }
-
-
-    static function move($path, $key){
-        $kako_dir = dirname(self::kako_path($path, $key));
-        if(!is_dir($kako_dir)){
-            mkdir($kako_dir, 0777, true);
-        }
-        rename(self::path($path, $key), self::kako_path($path, $key));
-        Subject::delete($path, $key);
-    }
+    rename($datFile, $kakoFile);
+    subject_delete($bbsDir, $key);
 }
 
 
-
-class Res{
-    static function from($from){
-        if(ADMIN){
-            return ADMIN['name'] . ' ★';
+function subject_select($content, $key){
+    $marker = "$key.";
+    foreach($content as $k => $v){
+        if(str_starts_with($v, $marker)){
+            preg_match('/<>(.+?) \((\d+)\)$/', $v, $m);
+            return (object)['index'=>$k, 'key'=>$key, 'subject'=>$m[1], 'num'=>$m[2]];
         }
-        if($from === ''){
-            return SET['nanashi'];
-        }
-        $from = self::escape($from);
-        $from = str_replace('★', '☆', $from);
-        $from = str_replace('◆', '◇', $from);
-        $from = preg_replace_callback('/#(.+)/', function($m){ return ' ◆</b>'.self::trip($m[1]).'<b>'; }, $from);
-
-        return $from;
-    }
-
-
-    static function mail($mail){
-        $mail = preg_replace('/#.*/', '', $mail);
-        return self::escape($mail);
-    }
-
-
-    static function date($time){
-        $week = ['日','月','火','水','木','金','土'][date('w', $time)];
-        return date("Y/m/d($week) H:i:s", $time);
-    }
-
-
-    static function subject($subject){
-        return self::escape($subject);
-    }
-
-
-    static function message($message){
-        return self::escape(rtrim($message), '<br>');
-    }
-
-
-    static function save($path, $key, $from, $mail, $message){
-        $date = self::date($_SERVER['REQUEST_TIME']);
-        $dat  = mb_convert_encoding("$from<>$mail<>$date<> $message <>\n", 'sjis', 'utf-8');
-
-        return file_edit(Subject::path($path), function($file) use($path, $key, $dat){
-            $selected = Subject::select($file, $key);
-            if(!$selected){
-                error('このスレッドは存在しません');
-            }
-            if($selected->num >= 1000){
-                error('このスレッドにはこれ以上書き込めません');
-            }
-
-            file_put_contents(Thread::path($path, $key), $dat, LOCK_EX|FILE_APPEND);
-
-            $selected->num++;
-            array_splice($file, $selected->index, 1);
-            array_unshift($file, "$key.dat<>$selected->subject ($selected->num)\n");
-
-            return $file;
-        });
-    }
-
-
-    static function delete($path, $key, $num){
-        $dat_path = Thread::exists($path, $key) ? Thread::path($path, $key) : Thread::kako_path($path, $key);
-        file_edit($dat_path, function($file) use($num){
-            $file[$num-1] = mb_convert_encoding("あぼーん<>あぼーん<>あぼーん<>あぼーん<>\n", 'sjis', 'utf-8');
-            return $file;
-        });
-    }
-
-
-    static function trip($tripkey){
-        if(strlen($tripkey) >= 12){
-            return str_replace('+', '.', substr(base64_encode(sha1($tripkey, true)), 0, 12));
-        }
-        else{
-            $salt = preg_replace('/[^.-z]/', '.', substr($tripkey.'H.', 1, 2));
-            $map  = [':'=>'A', ';'=>'B', '<'=>'C', '='=>'D', '>'=>'E', '?'=>'F', '@'=>'G', '['=>'a', '\\'=>'b', ']'=>'c', '^'=>'d', '_'=>'e', '`'=>'f'];
-            return substr(crypt($tripkey, strtr($salt, $map)), -10);
-        }
-    }
-
-
-    static function id($bbs = ''){
-        $ip   = substr(getenv('REMOTE_ADDR'), -5);
-        $date = date('Ymd');
-        $pass = __FILE__;
-        $md5  = md5($ip.$date.$pass.$bbs);
-
-        return substr($md5, 0, 10);
-    }
-
-
-    static function escape($str, $br = ''){
-        $str = htmlspecialchars($str, ENT_QUOTES, 'UTF-8', false);
-        $str = str_replace("\n", $br, $str);
-        $str = preg_replace('/[[:cntrl:]]/', '', $str);
-
-        return $str;
     }
 }
 
+function subject_delete($bbsDir, $key){
+    file_edit("$bbsDir/subject.txt", fn($content) => array_filter($content, fn($v) => !str_starts_with($v, "$key.")));
+}
 
 
-class Maintenance{
+function dat_from($from, $default, $admin){
+    if($admin)       return $admin['name'] . ' ★';
+    if($from === '') return $default;
 
-    static function 削除($path, $message, $key = null){
-        foreach(explode("\n", $message) as $v){
-            if(preg_match("/>>(\d+)-?(\d+)?/", $v, $m)){
-                if($m[1] == 1){
-                    return '1番目のレスは削除できません';
-                }
-                $m[2] = $m[2] ?? $m[1];
-                for($i = $m[1]; $i <= $m[2]; $i++){
-                    Res::delete($path, $key, $i);
-                }
-            }
-            else if(preg_match("|/(\d+)/$|", $v, $m)){
-                Thread::delete($path, $m[1]);
-            }
-        }
-        return '削除しました';
+    $from = escape($from);
+    $from = str_replace('★', '☆', $from);
+    $from = str_replace('◆', '◇', $from);
+    $from = preg_replace_callback('/#(.+)/', fn($m)=>' ◆</b>'.self::trip($m[1]).'<b>', $from);
+
+    return $from;
+}
+
+function dat_mail($mail){
+    $mail = preg_replace('/#.*/', '', $mail);
+    return escape($mail);
+}
+
+function dat_message($message){
+    return escape(rtrim($message), true);
+}
+
+function dat_subject($subject){
+    return escape($subject);
+}
+
+function dat_date(){
+    $week = ['日','月','火','水','木','金','土'][date('w', $_SERVER['REQUEST_TIME'])];
+    return date("Y/m/d($week) H:i:s", $_SERVER['REQUEST_TIME']);
+}
+
+function trip($tripkey){
+    if(strlen($tripkey) >= 12){
+        return str_replace('+', '.', substr(base64_encode(sha1($tripkey, true)), 0, 12));
     }
-
-
-    static function 復帰($path){
-        file_edit(Subject::path($path), function($file) use($path){
-            foreach(glob("$path/dat/*.dat") as $v){
-                $list[$v] = filemtime($v);
-            }
-            arsort($list);
-
-            foreach(array_keys($list) as $v){
-                $filename = basename($v);
-                $dat      = file($v);
-                $subject  = explode("<>", $dat[0])[4];
-                $subject  = rtrim($subject);
-                $count    = count($dat);
-                $result[] = "$filename<>$subject ($count)\n";
-            }
-
-            return $result;
-        });
-        return '復帰完了';
+    else{
+        $salt = preg_replace('/[^.-z]/', '.', substr($tripkey.'H.', 1, 2));
+        $map  = [':'=>'A', ';'=>'B', '<'=>'C', '='=>'D', '>'=>'E', '?'=>'F', '@'=>'G', '['=>'a', '\\'=>'b', ']'=>'c', '^'=>'d', '_'=>'e', '`'=>'f'];
+        return substr(crypt($tripkey, strtr($salt, $map)), -10);
     }
+}
 
+function id($bbs = ''){
+    $ip   = substr(getenv('REMOTE_ADDR'), -5);
+    $date = date('Ymd');
+    $pass = __FILE__;
+    $md5  = md5($ip.$date.$pass.$bbs);
 
-
-    static function 倉庫($path, $message){
-        foreach(explode("\n", $message) as $v){
-            if(preg_match("|/(\d+)/$|", $v, $m)){
-                Thread::move($path, $m[1]);
-            }
-        }
-        return '倉庫へ移動しました';
-    }
+    return substr($md5, 0, 10);
 }
 
 
 function post($name){
-    return $_POST[$name] ?? '';
+    return $_POST[$name] ?? "";
 }
-
 
 function isUTF8($str){
     return preg_match('//u', $str);
 }
 
+function escape($str, $br = false){
+    $br  = $br ? '<br>' : '';
+    $str = htmlspecialchars($str);
+    $str = str_replace("\n", $br, $str);
+    $str = preg_replace('/[[:cntrl:]]/', '', $str);
+
+    return $str;
+}
 
 function file_edit($file, $fn){
     $fp = fopen($file, 'cb+');
-    if(!$fp){
-        return false;
-    }
+    if(!$fp) return false;
     flock($fp, LOCK_EX);
 
-    $contents = [];
     while(($line = fgets($fp)) !== false){
-        $contents[] = $line;
+        $content[] = $line;
     }
 
-    $contents = $fn($contents);
+    $content = $fn($content ?? []);
 
-    if(is_array($contents)){
-        $contents = implode('', $contents);
+    if(is_array($content)){
         ftruncate($fp, 0);
         rewind($fp);
-        fwrite($fp, $contents);
-        flock($fp, LOCK_UN);
+        fwrite($fp, implode('', $content));
         fclose($fp);
         return true;
     }
     else{
-        flock($fp, LOCK_UN);
         fclose($fp);
         return false;
     }
+}
+
+function setting_load(){
+    include './setting.php';
+    return get_defined_vars();
+}
+
+function setting_admin($setting, $mail){
+    $i = array_search($mail, array_column($setting['admin'], 'password'));
+
+    return is_int($i) ? $setting['admin'][$i] : false;
 }
 
 
@@ -403,7 +290,6 @@ function ok($bbs, $key){
     exit;
 }
 
-
 function error($str){
     header('Cache-Control: no-cache');
     header('Content-Type: text/html; charset=shift_jis');
@@ -415,3 +301,53 @@ function error($str){
     exit;
 }
 
+
+function 管理削除($c){
+    foreach(explode("\n", $c->message) as $v){
+        if(preg_match("/>>(\d+)-?(\d+)?/", $v, $m)){
+            if($m[1] == 1) return '1番目のレスは削除できません';
+
+            $m[2] = $m[2] ?? $m[1];
+            foreach(range($m[1], $m[2]) as $i){
+                res_delete($c->datFile, $c->kakoFile, $i);
+            }
+        }
+        else if(preg_match("|/(\d+)/$|", $v, $m)){
+            thread_delete($c->bbsDir, $m[1]);
+        }
+    }
+    return '削除しました';
+}
+
+
+
+function 管理倉庫($c){
+    foreach(explode("\n", $c->message) as $v){
+        if(preg_match("|/(\d+)/$|", $v, $m)){
+            thread_move($c->bbsDir, $m[1]);
+        }
+    }
+    return '倉庫へ移動しました';
+}
+
+
+function 管理復帰($c){
+    file_edit($c->subjectFile, function() use($c){
+        foreach(glob("$c->bbsDir/dat/*.dat") as $v){
+            $list[$v] = filemtime($v);
+        }
+        arsort($list);
+
+        foreach(array_keys($list) as $v){
+            $filename = basename($v);
+            $dat      = file($v);
+            $subject  = explode("<>", $dat[0])[4];
+            $subject  = rtrim($subject);
+            $count    = count($dat);
+            $content[] = "$filename<>$subject ($count)\n";
+        }
+
+        return $content;
+    });
+    return '復帰完了';
+}
